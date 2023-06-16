75bece48141859d44c2f5b62e8d7f5e30d3df557
AHMED <ahmedalaa277143@gmail.com>
2023-06-16 09:34:31 -0700

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,15 +7,17 @@
  *
  * Return: 0 (success)
 */
+
 int main(void)
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-	putchar("\n");
-	return(0);
-		
+	putchar('\n');
+
+	return (0);
 }
