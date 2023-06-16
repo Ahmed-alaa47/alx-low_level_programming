b221cb5429592b85e1d53c9e255b3d344d5ca982
AHMED <ahmedalaa277143@gmail.com>
2023-06-16 10:44:50 -0700

    a

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,10 +14,10 @@ int main(void)
 
     while (ch <= 'z')
     {
-        if (ch == 'e' || ch == 'q')
-            ch++;
-        putchar(ch);
-        ch++;
+        	if (ch == 'e' || ch == 'q')
+           	        ch++;
+        	putchar(ch);
+        	ch++;
     }
     putchar('\n');
 
